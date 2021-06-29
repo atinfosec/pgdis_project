35f98b8b7ccc73715ed4fc933bed71a298a1c310
atinfosec <61945256+atinfosec@users.noreply.github.com>
2021-06-29 04:50:22 -0700

    Update server.php

--- Injection/code_injection/server.php
+++ Injection/code_injection/server.php
@@ -1,4 +1,13 @@
 <?php
-$user = $_GET['user'];
-eval("echo 'http://'.$user;");
+$page = $_GET['page'];
+include($page);		// Comment this for security
+
+// Uncomment lines give below for security
+// if($page == 'login.php'){
+// 	include($page);
+// }
+// else{
+// 	echo "Page cannot be requested";
+// }
+
 ?>
